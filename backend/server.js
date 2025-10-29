800a432f9adf71e65bb7eeff9be292a37c24f534
asifkfaiz <asifkfaiz@gmail.com>
2025-10-29 13:01:54 +0530

    f

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const cors=require('cors')
 require('dotenv').config()
+
 const port=process.env.PORT
 
 app.use(cors())
 app.use(express.json())
 mongoose.connect(process.env.MONGO_URI);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
 
 const userRoutes=require('./routes/userRoutes')
 const productRoutes=require('./routes/productRoutes')
