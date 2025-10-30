458562b2afbfc8c2a52ec61f481dffd464cec957
asifkfaiz <asifkfaiz@gmail.com>
2025-10-30 11:54:41 +0530

    f

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -1,8 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { productDetails } = require("../controllers/productController");
 
 const cartSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  productName: {
+    type: String,
+    required:true,
+  },
+  productPrice: {
+    type: String,
+    required:true,
+    trim: true,
+  },
+  productCategory: {
+    type: String,
+  },
+  productImage: {
+    type: String,
+    required: true,
+  },
+  productQuantity:{
+    type:Number,
+    required:true
+  }
+});
 
-})
-
-const Cart = mongoose.model('Cart', cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 module.exports = Cart;
